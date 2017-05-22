203c506a2db0f3820681e05dc51bd4acb24536cb
Felipe Navarro Balbino Alves <fnba@cesar.org.br>
2017-05-21 23:05:53 -0300

    Modified test_knn to remove rows from data

--- test_knn.py
+++ test_knn.py
@@ -12,13 +12,29 @@ print 'Getting set of outputs'
 
 [X_train, Y_train, X_test, Y_test] = pre_process_and_hold_out(X, Y)
 
+#eliminar var 5, 7, 8, 9
+(num_elems, dimensions) = np.shape(X_train)
+num_elems_test = np.shape(X_test)[0]
+
+selected_colums = range(dimensions)
+selected_colums.remove(0)
+selected_colums.remove(1)
+selected_colums.remove(2)
+# selected_colums.remove(5)
+# selected_colums.remove(7)
+# selected_colums.remove(8)
+# selected_colums.remove(9)
+
+n_X_train = X_train[np.ix_(range(num_elems), selected_colums)]
+n_X_test = X_test[np.ix_(range(num_elems_test), selected_colums)]
+
 accuracies = []
 
 for i in xrange(50):
     K = 2*i + 1
     knn_classifier = KNeighborsRegressor(n_neighbors=K)
-    knn_classifier.fit(X_train, Y_train)
-    new_accuracy = knn_classifier.score(X_test, Y_test)
+    knn_classifier.fit(n_X_train, Y_train)
+    new_accuracy = knn_classifier.score(n_X_test, Y_test)
     accuracies.append([K, new_accuracy])
 
 plt.xlabel('Valor de K')
