a34fc6a138e6e13280ddc9d545d061128658d807
Felipe Navarro Balbino Alves <fnba@cesar.org.br>
2017-05-23 08:08:20 -0300

    Use mean square error to evaluate the knn classifier

--- test_knn.py
+++ test_knn.py
@@ -3,6 +3,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 
 import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
 
 print 'Reading files'
 np_data = read_file()
@@ -17,29 +20,34 @@ print 'Getting set of outputs'
 num_elems_test = np.shape(X_test)[0]
 
 selected_colums = range(dimensions)
-selected_colums.remove(0)
-selected_colums.remove(1)
-selected_colums.remove(2)
-# selected_colums.remove(5)
-# selected_colums.remove(7)
-# selected_colums.remove(8)
-# selected_colums.remove(9)
+#selected_colums.remove(0)
+#selected_colums.remove(1)
+#selected_colums.remove(2)
+#selected_colums.remove(5)
+#selected_colums.remove(7)
+#selected_colums.remove(8)
+#selected_colums.remove(9)
 
 n_X_train = X_train[np.ix_(range(num_elems), selected_colums)]
 n_X_test = X_test[np.ix_(range(num_elems_test), selected_colums)]
 
 accuracies = []
-
+predictions = []
 for i in xrange(50):
     K = 2*i + 1
-    knn_classifier = KNeighborsRegressor(n_neighbors=K)
+    knn_classifier = KNeighborsRegressor(n_neighbors=K, weights='distance')
     knn_classifier.fit(n_X_train, Y_train)
-    new_accuracy = knn_classifier.score(n_X_test, Y_test)
+    new_accuracy = np.sqrt(mean_error(knn_classifier, n_X_test, Y_test))#knn_classifier.score(n_X_test, Y_test)
+    print 'Mean error', new_accuracy
+
     accuracies.append([K, new_accuracy])
+    predictions = knn_classifier.predict(n_X_test)
 
 plt.xlabel('Valor de K')
-plt.ylabel('Acuracia')
-x = [a for [a,b] in accuracies]
-y = [b for [a,b] in accuracies]
-plt.plot(x,y)
+plt.ylabel('Raiz do erro medio')
+plt.scatter(predictions, Y_test)
 plt.show()
+#x = [a for [a,b] in accuracies]
+#y = [b for [a,b] in accuracies]
+#plt.plot(x,y)
+#plt.show()
