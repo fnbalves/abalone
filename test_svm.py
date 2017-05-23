df6816f320827ea7b0e808d0c39ed94fb9e362b0
Felipe Navarro Balbino Alves <fnba@cesar.org.br>
2017-05-23 10:24:43 -0300

    Changed svm to use pca and mean error as an evaluation metric

--- test_svm.py
+++ test_svm.py
@@ -1,6 +1,7 @@
 from util import *
 from sklearn.svm import SVC
 from sklearn.svm import SVR
+from sklearn import decomposition
 
 import matplotlib.pyplot as plt
 
@@ -10,21 +11,31 @@ print 'Separate X and Y'
 [X, Y] = separate_X_Y(np_data)
 print 'Getting set of outputs'
 
+pca = decomposition.PCA(n_components=5)
+
 [X_train, Y_train, X_test, Y_test] = pre_process_and_hold_out(X, Y)
+pca.fit(X_train)
+n_X_train = pca.transform(X_train)
+n_X_test = pca.transform(X_test)
 
 accuracies = []
+predictions = []
 
-for i in xrange(50):
+for i in xrange(200):
     c = (5.0*(i + 1))/50.0
     print i
-    svm_classifier = SVR(C = c, kernel = 'rbf')
-    svm_classifier.fit(X_train, Y_train)
-    new_accuracy = svm_classifier.score(X_test, Y_test)
+    svm_classifier = SVR(C = c, kernel = 'linear')
+    svm_classifier.fit(n_X_train, Y_train)
+    new_accuracy = np.sqrt(mean_error(svm_classifier, n_X_test, Y_test))
+    print 'Erro medio', new_accuracy
     accuracies.append([c, new_accuracy])
+    predictions = svm_classifier.predict(n_X_test)
 
 plt.xlabel('Valor de C')
-plt.ylabel('Acuracia')
-x = [a for [a,b] in accuracies]
-y = [b for [a,b] in accuracies]
-plt.plot(x,y)
+plt.ylabel('Raiz do erro medio')
+plt.scatter(predictions, Y_test)
 plt.show()
+#x = [a for [a,b] in accuracies]
+#y = [b for [a,b] in accuracies]
+#plt.plot(x,y)
+#plt.show()
