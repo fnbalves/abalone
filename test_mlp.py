13e94713658dd1ad2f2c75b300d4d648aaf6bcdf
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-05-23 23:23:46 -0300

    Modified test_mlp to use the custom transformation

--- test_mlp.py
+++ test_mlp.py
@@ -1,6 +1,7 @@
 from util import *
 from sklearn.neural_network import MLPRegressor
 from sklearn.neural_network import MLPClassifier
+#from sklearn.linear_model import LinearRegression
 from sklearn import decomposition
 
 import matplotlib.pyplot as plt
@@ -11,12 +12,12 @@ print 'Separate X and Y'
 [X, Y] = separate_X_Y(np_data)
 print 'Getting set of outputs'
 
-pca = decomposition.PCA(n_components=5)
+#pca = decomposition.PCA(n_components=5)
 
 [X_train, Y_train, X_test, Y_test] = pre_process_and_hold_out(X, Y)
-pca.fit(X_train)
-n_X_train = pca.transform(X_train)
-n_X_test = pca.transform(X_test)
+#pca.fit(X_train)
+n_X_train = X_train #pca.transform(X_train)
+n_X_test = X_test #pca.transform(X_test)
 
 errors = []
 predictions = []
@@ -24,7 +25,7 @@ best_error = 10000
 for i in xrange(20):
     c = i + 1
     print i
-    mlp_classifier = MLPClassifier((c), activation='relu', solver='lbfgs', learning_rate='constant')
+    mlp_classifier = MLPRegressor((c), activation='relu', solver='lbfgs', learning_rate='constant')
     mlp_classifier.fit(n_X_train, Y_train)
     new_error = np.sqrt(mean_error(mlp_classifier, n_X_test, Y_test))
     print 'Mean error', new_error
@@ -39,7 +40,8 @@ for i in xrange(20):
 #y = [b for [a,b] in errors]
 #plt.plot(x,y)
 #plt.show()
-plt.xlabel('Valores do MLP')
-plt.ylabel('Valores originais')
-plt.scatter(predictions, Y_test)
-plt.show()
+
+#plt.xlabel('Valores do MLP')
+#plt.ylabel('Valores originais')
+#plt.scatter(predictions, Y_test)
+#plt.show()
