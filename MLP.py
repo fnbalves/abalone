Created functions to compute numeric_gradients and fixed backpropagation bugs and cost scale factor

MLP.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 
 class MLP(object):
-    def __init__(self, hidden_layers_sizes, lambda_reg=1, learning_rate=0.01, power_T=0.5, max_fail=10, min_increment=0.000000001, max_iter=500):
+    def __init__(self, hidden_layers_sizes, lambda_reg=1, learning_rate=0.0001, power_T=0.5, max_fail=10, min_increment=0.000000001, max_iter=500):
         self.hidden_layers_sizes = hidden_layers_sizes
         self.lambda_reg = lambda_reg
         self.power_T = power_T
@@ -62,7 +62,7 @@ class MLP(object):
             n_X.append(new_line)
         return np.asarray(n_X)
 
-    def forward(self, X):
+    def forward_with_test_W(self, X, W):
         shape_X = np.shape(X)
         if len(shape_X) == 1:
             X = np.reshape(X, (1, len(X)))
@@ -70,36 +70,77 @@ class MLP(object):
         activations = []
         current_act = X
         before_activation = X
-        num_layers = len(self.W)
+        num_layers = len(W)
         for i in xrange(num_layers):
             (num_elems, dims) = np.shape(current_act)
             bias = np.ones((num_elems, 1), dtype=np.float32)
             act_with_bias = np.append(bias, current_act, axis=1)
             activations.append((before_activation, act_with_bias))
-            before_activation = np.dot(act_with_bias, self.W[i])
+            before_activation = np.dot(act_with_bias, W[i])
             current_act = self.relu(before_activation)
+        return {'output' : current_act, 'activations' : activations, 'output_before' : before_activation}
 
-        return {'output' : current_act, 'activations' : activations}
-    
-    def cost(self):
+    def forward(self, X):
+        return self.forward_with_test_W(X, self.W)
+
+    def cost_with_test_W(self, W):
         (m, d) = np.shape(self.X_train)
         m = float(m)
-        Y_pred = np.reshape(self.forward(self.X_train)['output'], np.shape(self.Y_train))
-        first_term = np.mean((self.Y_train - Y_pred)**2)
+        Y_pred = np.reshape(self.forward_with_test_W(self.X_train, W)['output'], np.shape(self.Y_train))
+        first_term = np.sum((self.Y_train - Y_pred)**2)
         reg_term = 0
-        num_layers = len(self.W)
+        num_layers = len(W)
         for i in xrange(num_layers):
-            reg_term += np.sum(self.W[i]**2)
+            reg_term += np.sum(W[i]**2)
         
-        cost_val = (1.0/m)*(first_term) + (self.lambda_reg/(2.0*m))*reg_term
+        cost_val = (1.0/(2.0*m))*(first_term) + (self.lambda_reg/(2.0*m))*reg_term
         return cost_val
 
+    def cost(self):
+        return self.cost_with_test_W(self.W)
+    
+    def numeric_gradients(self):
+        Num_Deltas = []
+        num_layers = len(self.W)
+        test_W = []
+        for i in xrange(num_layers):
+            test_W.append(np.copy(self.W[i]))
+        
+        for i in xrange(num_layers):
+            new_grad = np.zeros(np.shape(self.W[i]))
+            pert_grad = np.zeros(np.shape(self.W[i]))
+            p = 0.0001
+            (ne,d) = np.shape(new_grad)
+            for l in xrange(ne):
+                for c in xrange(d):
+                    pert_grad[l, c] = p
+                    
+                    backup_W = np.copy(test_W[i])
+                    
+                    test_W[i] += pert_grad
+                    cost1 = self.cost_with_test_W(test_W)
+                    
+                    test_W[i] = np.copy(backup_W)
+                    test_W[i] -= pert_grad
+                    cost2 = self.cost_with_test_W(test_W)
+
+                    grad = (cost1 - cost2)/(2*p)
+                    new_grad[l,c] = grad
+
+                    pert_grad[l,c] = 0
+                    test_W[i] = np.copy(backup_W)
+                    
+            Num_Deltas.append(new_grad)
+
+        return Num_Deltas
+    
     def predict(self, X):
         return self.forward(X)['output']
-    
+        
     def backprop(self):
         Big_deltas = []
         num_layers = len(self.W)
+
         for i in xrange(num_layers):
             Big_deltas.append(np.zeros(np.shape(self.W[i])))
             
@@ -110,14 +151,14 @@ class MLP(object):
             y = self.Y_train[i]
             forw = self.forward(x)
             pred = forw['output']
+            pred_before = forw['output_before']
+            pred_der = np.transpose(self.der_relu(pred_before))
             activations = forw['activations']
 
-            final_delta = np.transpose(y - pred)
+            final_delta = np.multiply(np.transpose(pred - y), pred_der)
             last_delta = final_delta
             num_activations = len(activations)
-            deltas = []
-            deltas.append(final_delta)
-            
+
             for j in xrange(num_activations - 1):
                 curr_out = activations[num_activations - j - 1][0]
                 curr_act_with_bias = activations[num_activations - j -1][1]
@@ -125,21 +166,19 @@ class MLP(object):
                 Big_deltas[num_layers - j - 1] += np.dot(np.transpose(curr_act_with_bias), np.transpose(last_delta))
 
                 der = np.transpose(self.der_relu(curr_out))
-                
                 small_W = self.W[num_layers - j -1][1:, :]
                 
                 new_delta = np.multiply(np.dot(small_W, last_delta), der)
 
                 last_delta = new_delta
-                deltas.append(new_delta)
             curr_act_with_bias = activations[0][1]
             Big_deltas[0] += np.dot(np.transpose(curr_act_with_bias), np.transpose(last_delta))
 
         for i in xrange(num_layers):
             reg_term = np.copy(self.W[i])
             reg_term[:,0] *= 0
-            Big_deltas[i] *= (1.0/m)
             Big_deltas[i] += self.lambda_reg*reg_term
+            Big_deltas[i] *= (1.0/m)
 
         return Big_deltas
 
@@ -179,10 +218,12 @@ class MLP(object):
         if not has_exited_early:
             print 'Exited gradient descend by max iterations'
 
-X = np.array([[0,0], [0,1], [1,1], [1,0]]*100 + 100*[[1,1]], dtype=np.float32)
-Y = np.array([0,0,1,0]*100 + [1]*100, dtype=np.float32)
-mlp = MLPClassifier((3,), activation='relu', solver='lbfgs', learning_rate='invscaling')
-mlp.fit(X, Y)
-m = MLP((3,), power_T=0.5)
+#X = np.array([[0,0], [0,1], [1,1], [1,0]]*100 + 100*[[1,1]], dtype=np.float32)
+#Y = np.array([0,0,1,0]*100 + [1]*100, dtype=np.float32)
+X = np.array([[1,2,3],[3,2,1]], dtype=np.float32)
+Y = np.array([[1,2], [2,1]], dtype=np.float32)
+#mlp = MLPClassifier((3,), activation='relu', solver='lbfgs', learning_rate='invscaling')
+#mlp.fit(X, Y)
+m = MLP((3,), power_T=1, lambda_reg=0)
 m.fit(X, Y)
-m.gradient_descend()
+#m.gradient_descend()