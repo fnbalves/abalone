4997898efb0c9851350c81b46df20262d374ac23
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-06-22 23:47:01 -0300

    Modified test_knn_y_conv to use cross validation

--- test_knn_y_conv.py
+++ test_knn_y_conv.py
@@ -1,6 +1,6 @@
 from util import *
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import warnings
 
@@ -12,34 +12,31 @@ print 'Separate X and Y'
 [X, Y] = separate_X_Y(np_data)
 print 'Getting set of outputs'
 
-[X_train, Y_train_1, Y_train_2, Y_train_3, X_test, Y_test_1, Y_test_2, Y_test_3] = pre_process_and_hold_out(X, Y)
+[X_f, Y_no_transform, Y_equal_size, Y_equal_frequency] = pre_process_and_hold_out(X, Y)
 
 scores_1 = []
 scores_2 = []
 scores_3 = []
 
 Ks = []
+
 for i in xrange(50):
     K = 2*i + 1
     knn_classifier_1 = KNeighborsClassifier(n_neighbors=K)
     knn_classifier_2 = KNeighborsClassifier(n_neighbors=K)
     knn_classifier_3 = KNeighborsClassifier(n_neighbors=K)
-    knn_classifier_1.fit(X_train, Y_train_1)
-    knn_classifier_2.fit(X_train, Y_train_2)
-    knn_classifier_3.fit(X_train, Y_train_3)
     
-    predictions_1 = knn_classifier_1.predict(X_test)
-    predictions_2 = knn_classifier_2.predict(X_test)
-    predictions_3 = knn_classifier_3.predict(X_test)
-    new_score_1 = f1_score(Y_test_1, predictions_1, average='macro')
-    new_score_2 = f1_score(Y_test_2, predictions_2, average='macro')
-    new_score_3 = f1_score(Y_test_3, predictions_3, average='macro')
+    new_score_1 = np.mean(cross_val_score(knn_classifier_1, X_f, Y_no_transform, cv=10))
+    new_score_2 = np.mean(cross_val_score(knn_classifier_2, X_f, Y_equal_size, cv=10))
+    new_score_3 = np.mean(cross_val_score(knn_classifier_3, X_f, Y_equal_frequency, cv=10))
+    
     Ks.append(K)
     scores_1.append(new_score_1)
     scores_2.append(new_score_2)
     scores_3.append(new_score_3)
+    
 plt.xlabel('Valor de K')
-plt.ylabel('F1-score')
+plt.ylabel('Acuracia media - 10 fold')
 no_transf, = plt.plot(Ks, scores_1, 'b', label='sem transformacao')
 equal_length, = plt.plot(Ks, scores_2, 'r', label='tamanhos iguais')
 equal_frequency, = plt.plot(Ks, scores_3, 'g', label='frequencias iguais')
