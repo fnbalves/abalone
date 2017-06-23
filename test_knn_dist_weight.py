1638f65e87ccb36a8032b0b286ac162ee2996225
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-06-22 23:46:37 -0300

    Modified test_knn_dist_weight to use cross validation

--- test_knn_dist_weight.py
+++ test_knn_dist_weight.py
@@ -1,6 +1,6 @@
 from util import *
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import warnings
 
@@ -12,7 +12,7 @@ print 'Separate X and Y'
 [X, Y] = separate_X_Y(np_data)
 print 'Getting set of outputs'
 
-[X_train, Y_train_1, Y_train_2, Y_train_3, X_test, Y_test_1, Y_test_2, Y_test_3] = pre_process_and_hold_out(X, Y)
+[X_f, Y_no_transform, Y_equal_size, Y_equal_frequency] = pre_process_and_hold_out(X, Y)
 
 scores_1 = []
 scores_2 = []
@@ -22,21 +22,16 @@ for i in xrange(50):
     K = 2*i + 1
     knn_classifier_1 = KNeighborsClassifier(n_neighbors=K, weights='uniform')
     knn_classifier_2 = KNeighborsClassifier(n_neighbors=K, weights='distance')
-    knn_classifier_1.fit(X_train, Y_train_3)
-    knn_classifier_2.fit(X_train, Y_train_3)
     
-    predictions_1 = knn_classifier_1.predict(X_test)
-    predictions_2 = knn_classifier_2.predict(X_test)
-    
-    new_score_1 = f1_score(Y_test_3, predictions_1, average='macro')
-    new_score_2 = f1_score(Y_test_3, predictions_2, average='macro')
+    new_score_1 = np.mean(cross_val_score(knn_classifier_1, X_f, Y_equal_frequency, cv=10))
+    new_score_2 = np.mean(cross_val_score(knn_classifier_2, X_f, Y_equal_frequency, cv=10))
     
     Ks.append(K)
     scores_1.append(new_score_1)
     scores_2.append(new_score_2)
     
 plt.xlabel('Valor de K')
-plt.ylabel('F1-score')
+plt.ylabel('Acuracia media - 10 fold')
 uniform, = plt.plot(Ks, scores_1, 'b', label='uniform')
 distance, = plt.plot(Ks, scores_2, 'r', label='distance')
 
