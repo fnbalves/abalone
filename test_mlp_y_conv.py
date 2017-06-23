ba9561e5ebc76aace5284f6728acd47b3553043f
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-06-22 23:50:23 -0300

    Modified test_mlp_y_conv to use cross validation

--- test_mlp_y_conv.py
+++ test_mlp_y_conv.py
@@ -1,6 +1,7 @@
+import numpy as np
 from util import *
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import warnings
 from random_tansformations import *
@@ -15,7 +16,8 @@ print 'Separate X and Y'
 [X, Y] = separate_X_Y(np_data)
 print 'Getting set of outputs'
 
-[X_train, Y_train_1, Y_train_2, Y_train_3, X_test, Y_test_1, Y_test_2, Y_test_3] = pre_process_and_hold_out(X, Y)
+[X_f, Y_no_transform, Y_equal_size, Y_equal_frequency] = pre_process_and_hold_out(X, Y)
+
 t_X_train = apply_features(X_train, featureset)
 t_X_test = apply_features(X_test, featureset)
 
@@ -37,33 +39,25 @@ for i in xrange(20):
     mlp_classifier_3 = MLPClassifier((c), activation='relu', solver='lbfgs', learning_rate='constant')
     mlp_classifier_4 = MLPClassifier((c), activation='relu', solver='lbfgs', learning_rate='constant')
     
-    mlp_classifier_1.fit(X_train, Y_train_1)
-    mlp_classifier_2.fit(X_train, Y_train_2)
-    mlp_classifier_3.fit(X_train, Y_train_3)
-    mlp_classifier_4.fit(st_X_train, Y_train_3)
-    
-    predictions_1 = mlp_classifier_1.predict(X_test)
-    predictions_2 = mlp_classifier_2.predict(X_test)
-    predictions_3 = mlp_classifier_3.predict(X_test)
-    predictions_4 = mlp_classifier_4.predict(st_X_test)
-    
-    new_score_1 = f1_score(Y_test_1, predictions_1, average='macro')
-    new_score_2 = f1_score(Y_test_2, predictions_2, average='macro')
-    new_score_3 = f1_score(Y_test_3, predictions_3, average='macro')
-    new_score_4 = f1_score(Y_test_3, predictions_4, average='macro')
+    new_score_1 = np.mean(cross_val_score(mlp_classifier_1, X_f, Y_equal_frequency, cv=10))
+    new_score_2 = np.mean(cross_val_score(mlp_classifier_2, X_f, Y_equal_frequency, cv=10))
+    new_score_3 = np.mean(cross_val_score(mlp_classifier_3, X_f, Y_equal_frequency, cv=10))
+    #new_score_4 = np.mean(cross_val_score(mlp_classifier_4, X_f, Y_equal_frequency, cv=10))
     
     Ks.append(c)
     scores_1.append(new_score_1)
     scores_2.append(new_score_2)
     scores_3.append(new_score_3)
-    scores_4.append(new_score_4)
+    #scores_4.append(new_score_4)
     
 plt.xlabel('Tamanho da camada oculta')
-plt.ylabel('F1-score')
+plt.ylabel('Acuracias medias - 10 fold')
 no_transf, = plt.plot(Ks, scores_1, 'b', label='sem transformacao')
 equal_length, = plt.plot(Ks, scores_2, 'r', label='tamanhos iguais')
 equal_frequency, = plt.plot(Ks, scores_3, 'g', label='frequencias iguais')
-random_transf, = plt.plot(Ks, scores_4, 'y', label='features aleatorias')
+#random_transf, = plt.plot(Ks, scores_4, 'y', label='features aleatorias')
 
-plt.legend([no_transf, equal_length, equal_frequency, random_transf], ['sem transformacao', 'tamanhos iguais', 'frequencias iguais', 'features aleatorias'])
+#plt.legend([no_transf, equal_length, equal_frequency, random_transf], ['sem transformacao', 'tamanhos iguais', 'frequencias iguais', 'features aleatorias'])
+plt.legend([no_transf, equal_length, equal_frequency], ['sem transformacao', 'tamanhos iguais', 'frequencias iguais'])
+
 plt.show()
