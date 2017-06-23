9d96fdd0c00afe93875b208f11f9cabda56ae9c3
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-06-22 23:47:45 -0300

    Modified test_mlp_max_iter to use cross validation

--- test_mlp_max_iter.py
+++ test_mlp_max_iter.py
@@ -1,6 +1,6 @@
 from util import *
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import warnings
 
@@ -12,29 +12,24 @@ print 'Separate X and Y'
 [X, Y] = separate_X_Y(np_data)
 print 'Getting set of outputs'
 
-[X_train, Y_train_1, Y_train_2, Y_train_3, X_test, Y_test_1, Y_test_2, Y_test_3] = pre_process_and_hold_out(X, Y)
+[X_f, Y_no_transform, Y_equal_size, Y_equal_frequency] = pre_process_and_hold_out(X, Y)
 
 scores_1 = []
 scores_2 = []
 scores_3 = []
 scores_4 = []
+
 Ks = []
+
 for i in xrange(20):
     c = i + 1
     mlp_classifier_1 = MLPClassifier((c), activation='tanh', solver='lbfgs', learning_rate='invscaling', max_iter=100)
     mlp_classifier_2 = MLPClassifier((c), activation='tanh', solver='lbfgs', learning_rate='invscaling', max_iter=500)
     mlp_classifier_3 = MLPClassifier((c), activation='tanh', solver='lbfgs', learning_rate='invscaling', max_iter=1000)
-    mlp_classifier_1.fit(X_train, Y_train_3)
-    mlp_classifier_2.fit(X_train, Y_train_3)
-    mlp_classifier_3.fit(X_train, Y_train_3)
     
-    predictions_1 = mlp_classifier_1.predict(X_test)
-    predictions_2 = mlp_classifier_2.predict(X_test)
-    predictions_3 = mlp_classifier_3.predict(X_test)
-    
-    new_score_1 = f1_score(Y_test_3, predictions_1, average='macro')
-    new_score_2 = f1_score(Y_test_3, predictions_2, average='macro')
-    new_score_3 = f1_score(Y_test_3, predictions_3, average='macro')
+    new_score_1 = np.mean(cross_val_score(mlp_classifier_1, X_f, Y_equal_frequency, cv=10))
+    new_score_2 = np.mean(cross_val_score(mlp_classifier_2, X_f, Y_equal_frequency, cv=10))
+    new_score_3 = np.mean(cross_val_score(mlp_classifier_3, X_f, Y_equal_frequency, cv=10))
     
     Ks.append(c)
     scores_1.append(new_score_1)
@@ -42,7 +37,7 @@ for i in xrange(20):
     scores_3.append(new_score_3)
     
 plt.xlabel('Tamanho da camada oculta')
-plt.ylabel('F1-score')
+plt.ylabel('Acuracia media - 10 fold')
 iter_100, = plt.plot(Ks, scores_1, 'b', label='100 iter max')
 iter_500, = plt.plot(Ks, scores_2, 'r', label='500 iter max')
 iter_1000, = plt.plot(Ks, scores_3, 'g', label='1000 iter max')
