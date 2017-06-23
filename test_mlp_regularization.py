cdd8606f0918ad01a78a863125eb102746cc18bb
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-06-22 23:49:32 -0300

    Created code to test the mlp regularization parameter

---
+++ test_mlp_regularization.py
@@ -0,0 +1,35 @@
+from util import *
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
+
+print 'Reading files'
+np_data = read_file()
+print 'Separate X and Y'
+[X, Y] = separate_X_Y(np_data)
+print 'Getting set of outputs'
+
+[X_f, Y_no_transform, Y_equal_size, Y_equal_frequency] = pre_process_and_hold_out(X, Y)
+
+scores = []
+
+Ks = []
+
+for i in xrange(200):
+    c = float(i+1)/100.0
+    
+    mlp_classifier = MLPClassifier((10), activation='logistic', solver='lbfgs', learning_rate='constant', alpha=c)
+    
+    new_score = np.mean(cross_val_score(mlp_classifier, X_f, Y_equal_frequency, cv=10))
+    
+    Ks.append(c)
+    scores.append(new_score)
+    
+plt.xlabel('Regularization term')
+plt.ylabel('Acuracias medias - 10 fold')
+
+all_values, = plt.plot(Ks, scores, 'b')
+plt.show()
